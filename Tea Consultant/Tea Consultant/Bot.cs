b66f9297db87581d55407998c8ee4bb324da403e
Krimzon31 <83700861+Krimzon31@users.noreply.github.com>
2023-06-28 20:07:30 +0300

    Add files via upload

--- Tea Consultant/Tea Consultant/Bot.cs
+++ Tea Consultant/Tea Consultant/Bot.cs
@@ -1,36 +1,62 @@
-﻿namespace SignalR
+﻿using Npgsql.Internal.TypeHandlers.GeometricHandlers;
+using System.Data;
+using Tea_Consultant;
+
+namespace SignalR
 {
     public class Bot
     {
-
-        public List<(string, string)> dataFaleReader()
+        public static DBClass dbClass = new DBClass();
+        DataTable table = dbClass.Select();
+        /*
+        List<(string, string)> lines = new List<(string, string)>();
+        public List<(string, string)> DataTableToList()
         {
-            var lines = File.ReadAllLines("data.txt");
-            var questions = lines
-            .Select(s => s.Split('|'))
-            .Where(s => s.Length >= 2) // Добавленная проверка
-            .Select(s => (s[0], s[1]))
-            .ToList();
-            return questions;
-        }
 
+            public List<(string, string)> dataFileReader()
+            {
+
+                var lines = File.ReadAllLines("data.txt");
+                var questions = lines
+                .Select(s => s.Split('|'))
+                .Where(s => s.Length >= 2) // Добавленная проверка
+                .Select(s => (s[0], s[1]))
+                .ToList();
+
+
+                return questions;
+            }
+
+            return lines;
+        }
+        */
 
         public string otveBota(string message)
         {
             string otvet = "";
-            foreach (var question in dataFaleReader())
+            try
             {
-                if (message.Trim().ToLowerInvariant() == question.Item1.Trim().ToLowerInvariant().ToString())
+
+                for (int i = 0; i < table.Rows.Count; i++)
                 {
-                    otvet = question.Item2;
-                    break;
+                    string dbOtv = table.Rows[i][0].ToString();
+                    if (message.Trim().ToLowerInvariant() == dbOtv.Trim().ToLowerInvariant().ToString())
+                    {
+                        otvet = table.Rows[i][1].ToString();
+                        break;
+                    }
+                    else
+                    {
+                        otvet = "Это консультант немного глупый и не понял что вы имеете ввиду. Напишите пожалуйста так как попросили в предыдущем сообщении";
+                    }
                 }
-                else
-                {
-                    otvet = "Это консультант немного глупый и не понял что вы имеете ввиду. Напишите пожалуйста так как попросили в предыдущем сообщении";
-                }
+                return otvet;
             }
-            return otvet.ToString();
+            catch(Exception ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+                return otvet;
+            }
 
         }
     }
---
+++ Tea Consultant/Tea Consultant/DBClass.cs
@@ -0,0 +1,47 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace Tea_Consultant
+{
+    public class DBClass
+    {
+        private static string connstring = String.Format(
+           "Server={0};" +
+           "Port={1};" +
+           "User id={2};" +
+           "Password={3};" +
+           "Database={4};",
+
+           "localhost",
+           5432,
+           "postgres",
+           "310504",
+           "TeaCons");
+
+        private NpgsqlConnection connection = new NpgsqlConnection(connstring);
+        private string sql;
+        private NpgsqlCommand command;
+        private DataTable dataTable;
+
+        public DataTable Select()
+        {
+            try
+            {
+                connection.Open();
+                sql = @"select * from comand";
+                command = new NpgsqlCommand(sql, connection);
+                dataTable = new DataTable();
+                dataTable.Load(command.ExecuteReader());
+                connection.Close();
+                return dataTable;
+            }
+            catch (Exception ex)
+            {
+                connection.Close();
+                Console.WriteLine("Error: " + ex.Message);
+                return dataTable;
+            }
+        }
+        
+    }
+}
